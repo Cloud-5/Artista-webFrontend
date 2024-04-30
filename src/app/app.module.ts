c8580fb13a8d236380037a451686256bfd975881
BuddhiGayan2000 <108883603+BuddhiGayan2000@users.noreply.github.com>
2024-04-30 17:25:59 +0530

    purchase history fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxStarRatingModule } from 'ngx-star-rating';
 import { EditCustomerProfileComponent } from './pages/Customer/edit-customer-profile/edit-customer-profile.component';
 import { CustomerProfileGalleryComponent } from './pages/Customer/customer-profile-gallery/customer-profile-gallery.component';
 import { FollowingArtistsComponent } from './pages/Customer/following-artists/following-artists.component';
@@ -36,7 +35,6 @@ import { ArtistPortfolioService } from './pages/Customer/artist-portfolio/artist
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgxStarRatingModule,
     FormsModule,
     ReactiveFormsModule
   ],
--- src/app/pages/Customer/purchase-img-card/purchase-img-card.component.ts
+++ src/app/pages/Customer/purchase-img-card/purchase-img-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PurchaseHistoryService } from './purchase-history.service';
 
 @Component({
   selector: 'app-purchase-img-card',
@@ -6,24 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./purchase-img-card.component.css']
 
 })
-export class PurchaseImgCardComponent {
-  purchaseHistory = [
-    {
-      purchaseId: 1,
-      purchaseDatetime: '2024-02-24T16:00:00',
-      artworks: [
-        {
-          artwork_name: '3D Art',
-          artist_name: 'Artist A',
-          artwork_image: 'path/to/image1.jpg'
-        },
-        {
-          artwork_name: 'Motion Art',
-          artist_name: 'Artist B',
-          artwork_image: 'path/to/image2.jpg'
-        }
-      ]
-    },
-    // Add more purchase data as needed
-  ];
+export class PurchaseImgCardComponent implements OnInit {
+
+  constructor(
+    private purchaseHistoryService: PurchaseHistoryService
+  ){}
+
+  userId:number = 3
+  purchaseData: any[] =[];
+
+  
+  ngOnInit(): void {
+    this.loadPurchseHistory(this.userId);
+    console.log(this.purchaseData);
+  }
+
+  loadPurchseHistory(userId: number):void {
+    this.purchaseHistoryService.getPurchaseHistory(userId)
+    .subscribe((data: any[]) => {
+      this.purchaseData = data;
+      console.log('purchase data',this.purchaseData);
+    });
+  }
+
+
+  // purchaseHistory = [
+  //   {
+  //     purchaseId: 1,
+  //     purchaseDatetime: '2024-02-24T16:00:00',
+  //     artworks: [
+  //       {
+  //         artwork_name: '3D Art',
+  //         artist_name: 'Artist A',
+  //         artwork_image: 'path/to/image1.jpg'
+  //       },
+  //       {
+  //         artwork_name: 'Motion Art',
+  //         artist_name: 'Artist B',
+  //         artwork_image: 'path/to/image2.jpg'
+  //       }
+  //     ]
+  //   },
+  //   // Add more purchase data as needed
+  // ];
 }
