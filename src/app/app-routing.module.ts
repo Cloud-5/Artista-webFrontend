342875550145b307801c82b08226cc1084748af8
Kaumi Nethma <kauminethma@gmail.com>
2024-06-17 17:56:24 +0530

    new features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstForyouComponent } from './pages/first-foryou/first-foryou.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ForyouComponent } from './pages/foryou/foryou.component';
+import { ArtistComponent } from './pages/artist/artist.component';
+import { CheckoutFormComponent } from './pages/checkout-form/checkout-form.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstForyouComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'foryou', component: ForyouComponent },
+  { path: 'artist', component: ArtistComponent },
+  { path: 'checkout', component: CheckoutFormComponent},
+  { path: 'categories', component: CategoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtworkPreviewComponent } from './pages/artwork-preview/artwork-preview.component';
+import { HighResArtworkComponent } from './shared/components/high-res-artwork/high-res-artwork.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { ArtistsComponent } from './shared/cards/artists/artists.component';
+import { ArtsComponent } from './shared/cards/arts/arts.component';
+import { CartCardComponent } from './shared/cards/cart-card/cart-card.component';
+import { CategoryComponent } from './shared/cards/category/category.component';
+import { ArtistComponent } from './pages/artist/artist.component';
+import { FirstForyouComponent } from './pages/first-foryou/first-foryou.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ForyouComponent } from './pages/foryou/foryou.component';
+import { CheckoutFormComponent } from './pages/checkout-form/checkout-form.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtworkPreviewComponent,
+    HighResArtworkComponent,
+    FooterComponent,
+    NavbarComponent,
+    ArtistsComponent,
+    ArtsComponent,
+    CartCardComponent,
+    CategoryComponent,
+    ArtistComponent,
+    CartComponent,
+    FirstForyouComponent,
+    ForyouComponent,
+    CheckoutFormComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
