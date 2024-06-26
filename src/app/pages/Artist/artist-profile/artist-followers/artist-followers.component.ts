58ef28c12e0a19e675bd7237dc6f764ac1e77dd9
Dhanushkarangana <karunarathnarmdrr.21@uom.lk>
2024-06-26 14:50:23 +0530

    ...

--- src/app/pages/Artist/artist-profile/artist-followers/artist-followers.component.ts
+++ src/app/pages/Artist/artist-profile/artist-followers/artist-followers.component.ts
@@ -1,3 +1,40 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ArtistFollowersService } from './artist-followers.service';
+
+// @Component({
+//   selector: 'app-artist-followers',
+//   templateUrl: './artist-followers.component.html',
+//   styleUrls: ['./artist-followers.component.css']
+// })
+// export class ArtistFollowersComponent implements OnInit {
+//   followers: any[] = [];
+
+//   constructor(private followersService: ArtistFollowersService) { }
+
+//   ngOnInit(): void {
+//     this.loadFollowersData();
+//   }
+
+//   loadFollowersData() {
+//     this.followersService.getFollowersForArtist(1) // Replace 1 with actual artist id
+//       .subscribe((data: any) => {
+//         this.followers = data.followers;
+//       });
+//   }
+
+//   deleteFollower(followerId:number){
+//     this.followersService.deleteFollower(followerId,1) //replace 1 with actual artist id impliment thiszzzzzzzzzzz
+//     .subscribe(()=>{
+//      this.loadFollowersData();
+//     })
+//   }
+
+
+
+
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { ArtistFollowersService } from './artist-followers.service';
 
@@ -16,22 +53,16 @@ export class ArtistFollowersComponent implements OnInit {
   }
 
   loadFollowersData() {
-    this.followersService.getFollowersForArtists(1) // Replace 1 with actual artist id
-      .subscribe((data: Object) => {
-        this.followers = data as any[];
+    this.followersService.getFollowersForArtist(1) // Replace 1 with actual artist id
+      .subscribe((data: any) => {
+        this.followers = data.followers;
       });
   }
 
-  deleteFollowers(followerId:number){
-    this.followersService.deleteFollower(followerId,1) //replace 1 with actual artist id
-    .subscribe(()=>{
-     this.loadFollowersData();
-    })
+  deleteFollower(followerId: number) {
+    this.followersService.deleteFollower(followerId, 1) // replace 1 with actual artist id
+      .subscribe(() => {
+        this.loadFollowersData();
+      });
   }
-
-
-
-
 }
-
-
--- src/app/pages/Artist/artist-profile/artist-followers/artist-followers.service.ts
+++ src/app/pages/Artist/artist-profile/artist-followers/artist-followers.service.ts
@@ -1,31 +1,56 @@
-// artist-followers.service.ts
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { environment } from "../../../../../environments/environment"
 
-import { HttpClient } from '@angular/common/http';
+// @Injectable({
+//   providedIn:'root'
+// })
+// export class ArtistFollowersService{
+//   private apiUrl:string=environment.apiUrl;
+//   constructor(private http:HttpClient){}
+
+
+//   getFollowersForArtist(artistId:number){
+//     return this.http.get('${this.apiUrl}/artist/${artistId}/followers');
+//   }
+
+//   deleteFollower(followerId:number,artistId:number){
+//     return this.http.delete('${this.apiUrl}/user-management/delete-follower/${artistId}/${followerId}');
+//   }
+
+
+// }
+
+
+
+
 import { Injectable } from '@angular/core';
-import { environment } from './../../../../../environments/environment';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable,throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistFollowersService {
-  private apiUrl: string = environment.apiUrl;
+  private apiUrl = 'http://localhost:3000'; // Replace with your actual API endpoint
 
   constructor(private http: HttpClient) { }
 
-
-  getFollowersForArtists(artistId: number) {
-    return this.http.get(`${this.apiUrl}/user-management/followers/${artistId}`);
+  getFollowersForArtist(artistId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/artist-followers/${artistId}/followers`);
   }
 
-  deleteFollower(followerId: number, artistId: number) {
-    return this.http.delete(`${this.apiUrl}/user-management/delete-follower/${followerId}/${artistId}`);
+  deleteFollower(followerId: number, artistId: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/artist-followers/${artistId}/followers/${followerId}`)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
-
-  getFeedbackForArtist(artistId: number) {
-    return this.http.get(`${this.apiUrl}/user-management/feedback/${artistId}`);
-  }
+private handleError(error:HttpErrorResponse){
+console.error('an error occoured',error.message);
+return throwError('Something went wrong; please try again later.');
 }
 
 
-
-
+}
