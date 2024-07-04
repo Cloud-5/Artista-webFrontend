f63c841c9399d2f59eec96bf02ac6e7273b074df
Janani Uthpala Siriwardane <uthpala1217@gmail.com>
2024-07-04 10:31:16 +0530

    07/04 J commit 10.30

--- src/app/pages/Customer/art-card/art-card.component.ts
+++ src/app/pages/Customer/art-card/art-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Inject, PLATFORM_ID, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { CartItemService } from '../../../shared/cards/arts/arts.service';
 import { ArtCardServiceService } from './art-card-service.service';
 
 @Component({
@@ -10,10 +12,18 @@ export class ArtCardComponent {
   @Input() art: any;
   @Input() showRemoveButton: boolean = false;
   @Input() customerUserId!: string;
+  @Input() userId!: number; // Assuming userId is passed as an input
+  private isBrowser: boolean;
 
   @Output() removeGalleryItem = new EventEmitter<void>();
 
-  constructor(private artCardService: ArtCardServiceService) {}
+  constructor(
+    private artCardService: ArtCardServiceService,
+    private cartItemService: CartItemService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   formatLikeCount(likeCount: number): string {
     if (likeCount < 1000) {
@@ -26,14 +36,31 @@ export class ArtCardComponent {
   }
 
   removeFromGallery() {
-    console.log('art',this.art);
-    this.artCardService.removeGalleryItem(this.customerUserId, this.art.artwork_id).subscribe
-    (response => {
+    console.log('art', this.art);
+    this.artCardService.removeGalleryItem(this.customerUserId, this.art.artwork_id).subscribe(
+      response => {
         this.removeGalleryItem.emit();
         console.log(response.message);
         // Optionally remove the artwork from the UI or refresh the gallery
-      }, error => {
+      },
+      error => {
         console.error('Error removing artwork from gallery:', error);
-      });
+      }
+    );
+  }
+
+  addCart(art: any) {
+    if (this.isBrowser) {
+      this.cartItemService.addItem(this.userId, art.artwork_id).subscribe(
+        response => {
+          console.log('Item added to cart:', response);
+        },
+        error => {
+          console.error('Error adding item to cart:', error);
+        }
+      );
+    } else {
+      console.error('Cannot add to cart. HTTP requests are not supported in this environment.');
+    }
   }
 }
