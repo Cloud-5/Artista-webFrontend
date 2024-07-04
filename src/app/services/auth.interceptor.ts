593dc396a9c321e9b0d3c70cccbca000702c3309
BuddhiGayan2000 <108883603+BuddhiGayan2000@users.noreply.github.com>
2024-07-04 10:56:08 +0530

    interceptor modified

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -1,31 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private router: Router) { }
+
+    constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: any) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         
-        const token = localStorage.getItem('accessToken');
         let authReq = req;
-        console.log('authreq', authReq,'token',token);
-        if (token) {
-            authReq = req.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
+        if (isPlatformBrowser(this.platformId)) {
+            const token = localStorage.getItem('accessToken');
+            console.log('authreq', authReq, 'token', token);
+            if (token) {
+                authReq = req.clone({
+                    setHeaders: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+            }
         }
         
         return next.handle(authReq).pipe(
             catchError((error: HttpErrorResponse) => {
                 if (error.status === 401) {
-                    localStorage.removeItem('accessToken');
-                    localStorage.removeItem('admin_id');
+                    if (isPlatformBrowser(this.platformId)) {
+                        localStorage.removeItem('accessToken');
+                        localStorage.removeItem('admin_id');
+                    }
                     this.router.navigate(['/']);
                 }
                 return throwError(error);
