b9821e2985dd9d5426a71878a5083cd6ceec6b51
Gihan Shyavinda Jayathissa <jayathissakdgs.21@uom.lk>
2024-07-08 12:18:42 +0530

    ....

--- src/app/pages/chat/chat-screen/chat-screen.component.ts
+++ src/app/pages/chat/chat-screen/chat-screen.component.ts
@@ -23,6 +23,11 @@ export class ChatScreenComponent implements OnInit {
   customers$: Observable<Customer[]> = of([]); // List of customers who have sent messages
   selectedCustomerMessages$!: Observable<any[]>; // Messages from selected customer
   selectedMessageId: string = '';
+
+  filteredCustomers$: Observable<Customer[]> = of([]);
+  searchTerm: string = '';
+
+
   constructor(private chatService: ChatServiceService) {}
 
   ngOnInit(): void {
@@ -47,22 +52,48 @@ export class ChatScreenComponent implements OnInit {
     }
   }
 
-  loadCustomersWhoSentMessages(): void {
-    this.customers$ = this.chatService.getUniqueCustomersForArtist(this.senderId).pipe(
-      switchMap((customerIds: string[]) =>
-        from(customerIds).pipe(
-          mergeMap(id =>
-            this.chatService.getCustomerDetailsByUid(id).pipe(
-              map(customerDetails => ({
-                firebaseUid: id,
-                name: customerDetails ? customerDetails.displayName : `Customer`
-              }))
-            )
-          ),
-          toArray()
-        )
-  )
-);}
+//   loadCustomersWhoSentMessages(): void {
+//     this.customers$ = this.chatService.getUniqueCustomersForArtist(this.senderId).pipe(
+//       switchMap((customerIds: string[]) =>
+//         from(customerIds).pipe(
+//           mergeMap(id =>
+//             this.chatService.getCustomerDetailsByUid(id).pipe(
+//               map(customerDetails => ({
+//                 firebaseUid: id,
+//                 name: customerDetails ? customerDetails.displayName : `Customer`
+//               }))
+//             )
+//           ),
+//           toArray()
+//         )
+//   )
+// );}
+
+loadCustomersWhoSentMessages(): void {
+  this.customers$ = this.chatService.getUniqueCustomersForArtist(this.senderId).pipe(
+    switchMap((customerIds: string[]) =>
+      from(customerIds).pipe(
+        mergeMap(id =>
+          this.chatService.getCustomerDetailsByUid(id).pipe(
+            map(customerDetails => ({
+              firebaseUid: id,
+              name: customerDetails ? customerDetails.displayName : `Customer`
+            }))
+          )
+        ),
+        toArray()
+      )
+    )
+  );
+
+  this.filteredCustomers$ = this.customers$; // Initialize the filtered list with all customers
+}
+
+filterCustomers(): void {
+  this.filteredCustomers$ = this.customers$.pipe(
+    map(customers => customers.filter(customer => customer.name.toLowerCase().includes(this.searchTerm.toLowerCase())))
+  );
+}
 
   onCustomerSelect(customer: Customer): void {
     this.recipientId = customer.firebaseUid;
