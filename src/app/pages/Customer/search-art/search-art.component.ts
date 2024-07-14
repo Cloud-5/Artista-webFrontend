36793b52110725d25bc62cc1b0843dee73f85b21
Janani Uthpala Siriwardane <uthpala1217@gmail.com>
2024-07-14 18:56:08 +0530

    07/14 J commit

--- src/app/pages/Customer/search-art/search-art.component.ts
+++ src/app/pages/Customer/search-art/search-art.component.ts
@@ -37,7 +37,6 @@ export class SearchArtComponent implements OnInit {
           }
         );
       } else {
-        // Handle case when no search query is provided, perhaps load all artworks
       }
     });
 
